e7f4b872018c68accaaa79b1b4447075ea389782
Samah Khalid Bani Odeh <samahkhalid096@gmail.com>
2024-05-27 18:34:27 +0000

    Add code to measure total runtime

--- data/check_images.py
+++ data/check_images.py
@@ -26,6 +26,7 @@
 # Imports python modules
 from time import time, sleep
 
+
 # Imports print functions that check the lab
 from print_functions_for_lab_checks import *
 
@@ -40,7 +41,7 @@ from print_results import print_results
 # Main program function defined below
 def main():
     # TODO 0: Measures total program runtime by collecting start time
-    start_time = 
+    start_time = time()
     
     # TODO 1: Define get_input_args function within the file get_input_args.py
     # This function retrieves 3 Command Line Arugments from user as input from
@@ -116,10 +117,10 @@ def main():
     print_results(results, results_stats, None, True, True)
     
     # TODO 0: Measure total program runtime by collecting end time
-    end_time =
+    end_time = time()
     
     # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
-    tot_time = #calculate difference between end time and start time
+    tot_time = end_time - start_time #calculate difference between end time and start time
     print("\n** Total Elapsed Runtime:",
           str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
           +str(int((tot_time%3600)%60)) )
