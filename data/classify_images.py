a6b36ed6fac9682e4d8728f439337e7c4adef85b
Samah Bani Odeh <samahkhalid096@gmail.com>
2024-05-28 18:03:58 +0000

    Edit joining strings

--- data/classify_images.py
+++ data/classify_images.py
@@ -22,7 +22,7 @@
 ##
 # Imports classifier function for using CNN to classify images 
 from classifier import classifier 
-
+import os.path
 # TODO 3: Define classify_images function below, specifically replace the None
 #       below by the function definition of the classify_images function. 
 #       Notice that this function doesn't return anything because the 
@@ -65,14 +65,14 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    for key in results_dic:
-      model_label = classifier(images_dir + key, model)
+    for key , value in results_dic.items():
+      model_label = classifier(os.path.join(images_dir,key), model)
       model_label = model_label.lower().strip()
-      truth = results_dic[key][0]
+      truth = value[0]
       if truth in model_label:
-           results_dic[key].extend([model_label, 1])
+           value.extend([model_label, 1])
       else:
-           results_dic[key].extend([model_label, 0])
+           value.extend([model_label, 0])
 
         
 
